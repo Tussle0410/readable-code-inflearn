75197a1085d236ef4e4a86ad62045fc7b454824e
chanhyeonglee <ok.lee3@okestro.com>
2024-08-23 20:17:34 +0900

    3. 객체 (SOLID, DIP)

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -2,15 +2,32 @@ package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
         GameLevel gameLevel = new Middle();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-         Minesweeper minesweeper = new Minesweeper(gameLevel);
+         Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
          minesweeper.initialize();
          minesweeper.run();
     }
+    /**
+     *  DIP(Dependency Inversion Principle) : 의존성 역전
+     *
+     *  DI(Dependency Injection) : 의존성 주입 - "제 3자가 의존성을 주입해주는 것"
+     *  -> 주입은 IOC Container가 해준다.(Runtime시에 주입)
+     *  -> 또한, IoC Container는 객체의 생성 및 생명 주기를 관리하여, 개발자는 객체의 생성 및 생명 주기를 신경 쓰지 않아도 된다.
+     *  IoC(Inversion of Control) : 제어의 역전 - "제 3자가 제어권을 가지고 있는 것"
+     *  -> 프로그램의 제어 흐름을 개발자가 결정하는 것이 아니라 프레임워크에서 결정하는 것
+     *  -> 이미 만들어진 프레임 워크(Ex. Spring)에서 개발자가 코드를 작성해서 해당 프레임 워크를 실행하는 것(즉 실질적인 제어권을 프레임 워크가 가지고 있는 것)
+     *
+     */
 
 }
