d343a69c4bf778f6778272e303e36f23986ccbc9
chanhyeonglee <ok.lee3@okestro.com>
2024-08-29 20:28:03 +0900

    4. 객체 지향 적용 (숨겨진 도메인 정보 도출하기)

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,5 +1,6 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.Middle;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
@@ -10,11 +11,13 @@ import cleancode.minesweeper.tobe.io.OutputHandler;
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new Middle();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-         Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+        GameConfig gameConfig = new GameConfig(
+                new Middle(),
+                new ConsoleInputHandler(),
+                new ConsoleOutputHandler()
+        );
+         Minesweeper minesweeper = new Minesweeper(gameConfig);
          minesweeper.initialize();
          minesweeper.run();
     }
